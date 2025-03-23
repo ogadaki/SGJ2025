bd8d4be187d106987e10c8284a1db94c0d0aab99
Nicolas Decoster <ogadaki@mailbox.org>
2025-03-23 14:41:28 +0100

    Only one choice type for each question

--- Assets/Scripts/dictionnaireQuestions.cs
+++ Assets/Scripts/dictionnaireQuestions.cs
@@ -88,6 +88,63 @@ public class dictionnaireQuestions : MonoBehaviour
     }
 
     public void resetDico(){
+        choixNature = new List<Choix>
+        {
+            new Choix("La Théorie des Nuages", 1, 0, 0),
+            new Choix("Les Yeux de la Terre", 1, 0, 0),
+            new Choix("Le Dernier Printemps Sauvage", 1, 0, 0),
+            new Choix("Les Veilleurs du désert", 1, 0, 0),
+            new Choix("Murmures sous la Canopée", 1, 0, 0),
+            new Choix("Échos du Grand Fleuve", 1, 0, 0),
+            new Choix("Les Jardins du vivant", 1, 0, 0),
+            new Choix("La Fabrique du Printemps", 1, 0, 0),
+            new Choix("Le Cycle des Saisons", 1, 0, 0),
+            new Choix("La Mémoire des Océans", 1, 0, 0),
+            new Choix("Le Chant de la Forêt", 1, 0, 0),
+            new Choix("Ce que disent les Montagnes", 1, 0, 0),
+            new Choix("L'Odyssée de la Graine", 1, 0, 0),
+            new Choix("Le clan des loups", 1, 0, 0),
+            new Choix("Le Pouls de la Terre", 1, 0, 0)
+        };
+
+        choixTech = new List<Choix>
+        {
+            new Choix("L'Âge des Machines Sensibles", 0, 1, 0),
+            new Choix("Prothèses de l'Âme", 0, 1, 0),
+            new Choix("L'IA Entre Rêve et Réalité", 0, 1, 0),
+            new Choix("Mémoire et Données : Un Cerveau Collectif", 0, 1, 0),
+            new Choix("Le Grand Vertige Numérique", 0, 1, 0),
+            new Choix("Les Derniers Instants de l'Intelligence Biologique", 0, 1, 0),
+            new Choix("La machine consciente", 0, 1, 0),
+            new Choix("Les Enfants de l'IA", 0, 1, 0),
+            new Choix("Chroniques des Mondes Augmentés", 0, 1, 0),
+            new Choix("L'Homme qui Murmurait à l'Oreille des Robots", 0, 1, 0),
+            new Choix("Le Transhumanisme en Douceur", 0, 1, 0),
+            new Choix("Les Fictions du Progrès", 0, 1, 0),
+            new Choix("Réparer le Futur", 0, 1, 0),
+            new Choix("Le Grand Silence des Machines", 0, 1, 0),
+            new Choix("Les Robots Rêvent-ils d'Innocence ?", 0, 1, 0)
+        };
+
+        choixSocial = new List<Choix>
+        {
+            new Choix("Les Architectes du Lien", 0, 0, 1),
+            new Choix("Mémoires d'un Village Global", 0, 0, 1),
+            new Choix("Psychologie du Nouvel Homme Spatial", 0, 0, 1),
+            new Choix("L'Équilibre des Foules", 0, 0, 1),
+            new Choix("Ce Que Raconte une Poignée de Main", 0, 0, 1),
+            new Choix("Fragments d'un Monde en Partage", 0, 0, 1),
+            new Choix("Cartographie du tissu social", 0, 0, 1),
+            new Choix("Les Marchands de Joie", 0, 0, 1),
+            new Choix("Le Bruit des Connexions", 0, 0, 1),
+            new Choix("La Théorie des Cercles Sociaux", 0, 0, 1),
+            new Choix("Manuel de résolution des conflits", 0, 0, 1),
+            new Choix("Ce qui nous Lie face aux zombis", 0, 0, 1),
+            new Choix("Dernière Conversation Avant Minuit", 0, 0, 1),
+            new Choix("L'Expérience du Contact", 0, 0, 1),
+            new Choix("Théorie Générale de l'Entraide", 0, 0, 1)
+        };
+
         choixAll = choixNature.Concat(choixTech).Concat(choixSocial).ToList();
         GenerateNewQuestions();
     }
@@ -101,19 +158,19 @@ public class dictionnaireQuestions : MonoBehaviour
         question3 = new Question();
 
         question1.creerQuestion("Choisissez un livre");
-        question1.ajouterChoix1(GetAndRemoveRandom(choixAll));
-        question1.ajouterChoix2(GetAndRemoveRandom(choixAll));
-        question1.ajouterChoix3(GetAndRemoveRandom(choixAll));
+        question1.ajouterChoix1(GetAndRemoveRandom(choixNature));
+        question1.ajouterChoix2(GetAndRemoveRandom(choixTech));
+        question1.ajouterChoix3(GetAndRemoveRandom(choixSocial));
 
         question2.creerQuestion("Choisissez une chanson");
-        question2.ajouterChoix1(GetAndRemoveRandom(choixAll));
-        question2.ajouterChoix2(GetAndRemoveRandom(choixAll));
-        question2.ajouterChoix3(GetAndRemoveRandom(choixAll));
+        question2.ajouterChoix1(GetAndRemoveRandom(choixNature));
+        question2.ajouterChoix2(GetAndRemoveRandom(choixTech));
+        question2.ajouterChoix3(GetAndRemoveRandom(choixSocial));
 
         question3.creerQuestion("Choisissez un film");
-        question3.ajouterChoix1(GetAndRemoveRandom(choixAll));
-        question3.ajouterChoix2(GetAndRemoveRandom(choixAll));
-        question3.ajouterChoix3(GetAndRemoveRandom(choixAll));
+        question3.ajouterChoix1(GetAndRemoveRandom(choixNature));
+        question3.ajouterChoix2(GetAndRemoveRandom(choixTech));
+        question3.ajouterChoix3(GetAndRemoveRandom(choixSocial));
 
         currentQuestions[0] = question1;
         currentQuestions[1] = question2;
